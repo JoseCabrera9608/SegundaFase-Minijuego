fe25bb1b0a21b0efa8a7563abca323380a62c4d9
JoseCabrera9608 <Jecc9608@gmail.com>
2026-05-23 18:01:30 -0500

    BuildingSimpleScene && Implementing Input Reader
    Organizing Materials, Folders and Objects in Scene. Also implementing an inputReader script for more comfortable use of the InputSystem.

---
+++ Assets/_PROYECT/_SCRIPTS/InputReader.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputReader : MonoBehaviour, InputSystem_Actions.IPlayerActions
+{
+    public Vector2 move;
+    public Vector2 look;
+
+    private InputSystem_Actions inputSystem_Actions;
+
+    public static event Action onInteract;
+    void Start()
+    {
+        inputSystem_Actions = new InputSystem_Actions();
+        inputSystem_Actions.Player.SetCallbacks(this);
+        inputSystem_Actions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputSystem_Actions.Disable();
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (!context.performed) { return; }
+        onInteract?.Invoke();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        look = context.ReadValue<Vector2>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
+
+}
