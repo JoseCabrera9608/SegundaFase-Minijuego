e413d4f25b97bd928ed34c8a219fc0802316eab9
JoseCabrera9608 <Jecc9608@gmail.com>
2026-05-23 19:01:25 -0500

    Player Movement and Rotation
    PlayerMovement by mouseLook

---
+++ Assets/_PROYECT/_SCRIPTS/Player/MovementHandle.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class MovementHandle : MonoBehaviour
+{
+
+    private InputReader inputReader;
+    private PlayerData playerData;
+    private float lookRotation;
+    void Start()
+    {
+        inputReader = GetComponent<InputReader>();
+        playerData = GetComponent<PlayerData>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotationByMouse();
+        Movement();
+    }
+
+    private void Movement()
+    {
+        Vector3 move = transform.right* inputReader.move.x + transform.forward* inputReader.move.y;
+        move.y = 0f;
+        if(move.sqrMagnitude > 1f)
+        {
+            move.Normalize();
+        }
+        transform.position += move * playerData.playerSpeed * Time.deltaTime;
+    }
+
+    private void RotationByMouse()
+    {
+        transform.Rotate(Vector3.up * inputReader.look.x * playerData.mouseSensitivity);
+        lookRotation += (-inputReader.look.y * playerData.mouseSensitivity);
+        lookRotation = Mathf.Clamp(lookRotation, -playerData.minLookRotation, playerData.maxLookRotation);
+        playerData.camHolder.eulerAngles = new Vector3 (lookRotation, playerData.camHolder.transform.eulerAngles.y, playerData.camHolder.transform.eulerAngles.z);
+    }
+
+
+}
---
+++ Assets/_PROYECT/_SCRIPTS/Player/PlayerData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public float playerSpeed;
+    [Range(0f,1f)]
+    public float mouseSensitivity;
+    public float minLookRotation = 80f;
+    public float maxLookRotation = 80f;
+    public Transform camHolder;
+}
